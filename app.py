3564c7dc6e9e59e6ed8ba4784740c5efad797140
bwmurunga <bwmurunga@gmail.com>
2018-08-21 18:59:43 +0300

    Unit tests foe api endpoints

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+
+def create_app():
+    app = Flask(__name__)
+    api = Api(app)
+    
+    return app
--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,75 +1,63 @@
-import pytest
-from api.v1.views import get_questions
+from unittest import TestCase
+from app import create_app
+import json
 
-#  Test if endpoint returns all questions
-def test_get_all_questions(client):
-    expected_result = {
-    "questions": [
-    {
-        'id': 1,
-        'question': 'What is linting?',
-        'answers': [{'one': 'Showing syntax errors'}, {'two': 'Highlight style error'}]
-    },
-    {
-        'id': 2,
-        'question': 'What is TDD?',
-        'answers': [{'one': 'Writing tests'}, {'two': 'Writing tests before application code'}]
-    }
-    ]
-}
-    response = client.get('/api/v1/questions')
-    assert response.status_code == 200
-    assert b'"id":1' in response.data
-    assert b'"id":2' in response.data
-    assert expected_result == response.get_json()
+class BaseTestCase(TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.test_client = self.app.test_client()
+
+class AllQuestionsTestCase(BaseTestCase):
+    def test_getting_all_questions(self):
+        result = self.test_client.get('/api/v1/questions')
+        result_data = json.loads(result.data)
+
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(result_data['message'], 'Dictionary of questions')
 
 
-#  Test if endpoint returns specified question
-def test_fetch_question(client):
-    expected_result = {
-    "question": {
-        "answers": [{
-            "one": "Writing tests"},
-            {"two": "Writing tests before application code"
-        }],
-        "id": 2,
-        "question": "What is TDD?"
-    }
-}
-    response = client.get('/api/v1/questions/2')
-    assert response.status_code == 200
-    assert expected_result == response.get_json()
+class SpecificQuestion(BaseTestCase):
+    def test_indvidual_question(self):
+        result = self.test_client.get('/api/v1/questions/2')
+        result_data = json.loads(result.data)
+
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(result_data['message'], 'Dictionary containing question')
 
 
-# Test if question is posted and status is 201(created)
-def test_post_question(client):
-    response = client.post('/api/v1/questions', json={
-        "question": "Which python test types do you know?"
-        })
-    assert response.status_code == 201
-    assert b'Which python test types do you know?' in response.data
+class PostQuestion(BaseTestCase):
+    def test_posting_question(self):
+        result = self.test_client.get('/api/v1/questions')
+        result_data = json.loads(result.data)
+
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(result_data['message'], 'Dictionary containing question')
 
 
-# Test if answer to particular question is posted
-def test_post_answer(client):
-    expected_result = {
-    "question": {
-        "answers": [
-            {
-                "one": "Showing syntax errors"
-            },
-            {
-                "two": "Highlight style error"
-            },
-            {
-                "three": "New answer"
-            }
-        ],
-        "id": 1,
-        "question": "What is linting?"
-    }
-}
-    response = client.post('/api/v1/questions/2/answer', json={'three': 'New answer'}
-        )
-    assert response.status_code == 200
-    assert expected_result == response.get_json()
+class PostAnswer(BaseTestCase):
+    def test_posting_answer(self):
+        result = self.test_client.get('/api/v1/questions/2/answers')
+        result_data = json.loads(result.data)
+
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(result_data['message'], 'List containing answer')
+
+
+class PreferAnswer(BaseTestCase):
+    def test_preferring_answers(self):
+        result = self.test_client.get('/api/v1/questions/2/answers/2')
+        result_data = json.loads(result.data)
+
+        self.assertEqual(result.status_code, 201)
+        self.assertEqual(result_data['message'], 'Dictionary containing question')
+
+    def test_user_marking_authorization(self):
+        result  = self.test_client.get('/api/v1/questions/2/answers/2?username=""')
+        result_data = json.loads(result.data)
+
+        self.assertEqual(result_data.status_code, 401)
+        self.assertEqual(result_data['message', 'You are not authorized to mark or accept answer'])
+
+
+if __name__ == '__main__':
+    unittest.main()
