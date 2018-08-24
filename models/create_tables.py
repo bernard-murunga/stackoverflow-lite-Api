e503c8f8a0f490a9917be822d30e14432443d958
bwmurunga <bwmurunga@gmail.com>
2018-08-24 11:57:49 +0300

    Change db configurations

--- models/create_tables.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-# conn = psycopg2.connect(database="stackoverflow_lite", user = "postgres",
-#  password = "root", host = "127.0.0.1", port = "5432")
-conn = psycopg2.connect(database="d2kca9k4ga3sc",
- user = "llzutrwvpgezqo", password = "a5d9584ecbf382d446d607c4830b8c797a34422e468d78ef519fadcd3bb0c600",
-  host = "ec2-54-235-242-63.compute-1.amazonaws.com", port = "5432")
-print("Opened database successfully")
